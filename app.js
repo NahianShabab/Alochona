85c78ad8c76face3a27228e90917b769ac38f081
NahianShabab <nsnbd134@gmail.com>
2022-05-13 23:11:35 +0600

    Added rate limiter

--- app.js
+++ app.js
@@ -3,13 +3,26 @@ const database=require('./services/database/databaseAPI');
 const baseRoute=require('./baseRoute');
 const express=require("express");
 const app=express();
-
+const cors=require('cors');
+const rateLimit=require('express-rate-limit');
 
 
 // initialize necessary things here before starting the server 
 async function initialize(){
     dotenv.config();
+    
+    //init database
     await database.createPool();
+    
+    app.use(cors());
+    
+    //use rate limiter 
+    const limiter=rateLimit({
+    windowMs:process.env.REQUEST_WINDOW_MINUTE*60*1000,
+    max:process.env.MAX_REQUEST_PER_WINDOW
+    });
+    app.use(limiter);
+
 }
 
 
@@ -20,13 +33,13 @@ then(
     ()=>{
 
         app.use('/api',baseRoute);
-        
+
         app.listen(process.env.PORT,()=>{
             console.log('listening at port : '+process.env.PORT);
         })
     },
     (reason)=>{
-        console.log('error while initialize: '+reason);
+        console.log('error while initialize: reason: '+reason);
     }
 );
 
--- baseRoute.js
+++ baseRoute.js
@@ -3,12 +3,7 @@ const router=express.Router();
 const database=require('./services/database/databaseAPI');
 
 router.get('/',(req,res)=>{
-    res.send('hello from base router!');
-})
-
-router.get('/data',async (req,res)=>{
-    var result=await database.executeQuery('SELECT * FROM BOOK',{});
-    res.send(result);
+    res.status(200).send("hello from base router!");
 })
 
 
